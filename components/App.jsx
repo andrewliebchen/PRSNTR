582a43a6acd003a6922fe0ca7c04f3980aeddba6
Andrew Liebchen <andrewliebchen@gmail.com>
2016-07-31 21:46:28 -0700

    Add React Router

--- components/App.jsx
+++ components/App.jsx
@@ -1,85 +1,13 @@
 import React, { Component } from 'react';
-import { Meteor } from 'meteor/meteor';
-import { createContainer } from 'meteor/react-meteor-data';
-import { Presentations } from '../api/main';
+import { Router, Route, browserHistory } from 'react-router';
+import Presentation from './Presentation.jsx';
 
-class App extends Component {
-  _canAdvance() {
-    const { presentation } = this.props;
-    return presentation.slides.length - 1 > presentation.currentSlide;
-  };
-
-  _canReverse() {
-    const { presentation } = this.props;
-    return presentation.currentSlide > 0;
-  }
-
+export default class App extends Component {
   render() {
-    const { dataIsReady, presentation } = this.props;
-    const slidesLength = dataIsReady ? presentation.slides.length : 0;
-    const slidesStyle = {
-      width: `${slidesLength * 100}vw`,
-      transform: `translateX(-${presentation.currentSlide * 100}vw)`
-    };
-
-    if (!dataIsReady) {
-      return <div>Loading...</div>;
-    }
-
     return (
-      <div className="container">
-        <div className="slides" style={slidesStyle}>
-          {presentation.slides.map((slide, i) =>
-            <div className="slide" key={i}>
-              {slide}
-            </div>
-          )}
-        </div>
-        <div className="slides__meta">
-          <div className="slides__label">
-            Slide {presentation.currentSlide + 1} / {slidesLength}
-          </div>
-          <div className="slides__actions">
-            <button
-              onClick={this.handlePrevSlide.bind(this)}
-              disabled={!this._canReverse()}>
-              Prev
-            </button>
-            <button
-              onClick={this.handleNextSlide.bind(this)}
-              disabled={!this._canAdvance()}>
-              Next
-            </button>
-          </div>
-        </div>
-      </div>
+      <Router history={browserHistory}>
+        <Route path="/:id" component={Presentation}/>
+      </Router>
     );
   }
-
-  handleNextSlide() {
-    const { presentation } = this.props;
-    if (this._canAdvance()) {
-      Presentations.update(presentation._id, {
-        $inc: { currentSlide: 1 }
-      });
-    }
-  }
-
-  handlePrevSlide() {
-    const { presentation } = this.props;
-    if (this._canReverse()) {
-      Presentations.update(presentation._id, {
-        $inc: { currentSlide: -1 }
-      });
-    }
-  }
 }
-
-export default createContainer(() => {
-  const dataHandle = Presentations.findOne();
-  const dataIsReady = dataHandle;
-  return {
-    dataIsReady,
-    presentation: dataIsReady ? Presentations.findOne() : [],
-  };
-}, App);
