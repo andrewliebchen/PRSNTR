7aeba90241a7032d0682f3e65ba1e2a53e792a27
Andrew Liebchen <andrewliebchen@gmail.com>
2016-07-31 17:41:14 -0700

    Next/prev buttons

--- components/App.jsx
+++ components/App.jsx
@@ -4,12 +4,34 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Presentations } from '../api/main';
 
 class App extends Component {
+  _canAdvance() {
+    const { presentation } = this.props;
+    return presentation.slides.length > presentation.currentSlide;
+  };
+
+  _canReverse() {
+    const { presentation } = this.props;
+    return presentation.currentSlide > 0;
+  }
+
   render() {
     const { presentation } = this.props;
     if (presentation) {
       return (
-        <div onClick={this.handleAdvanceSlide.bind(this)}>
+        <div>
           Slide {presentation.currentSlide} / {presentation.slides.length}
+          <div>
+            <button
+              onClick={this.handlePrevSlide.bind(this)}
+              disabled={!this._canReverse()}>
+              Prev
+            </button>
+            <button
+              onClick={this.handleNextSlide.bind(this)}
+              disabled={!this._canAdvance()}>
+              Next
+            </button>
+          </div>
         </div>
       );
     } else {
@@ -17,11 +39,22 @@ class App extends Component {
     }
   }
 
-  handleAdvanceSlide() {
+  handleNextSlide() {
     const { presentation } = this.props;
-    Presentations.update(presentation._id, {
-      $inc: { currentSlide: 1 }
-    });
+    if (this._canAdvance()) {
+      Presentations.update(presentation._id, {
+        $inc: { currentSlide: 1 }
+      });
+    }
+  }
+
+  handlePrevSlide() {
+    const { presentation } = this.props;
+    if (this._canReverse()) {
+      Presentations.update(presentation._id, {
+        $inc: { currentSlide: -1 }
+      });
+    }
   }
 }
 
