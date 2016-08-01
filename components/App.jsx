7fb71302bebeb4668b15f93ce899e69dc50bdaeb
Andrew Liebchen <andrewliebchen@gmail.com>
2016-07-31 22:10:59 -0700

    Stub in admin

--- components/App.jsx
+++ components/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Router, Route, browserHistory } from 'react-router';
 import Presentation from './Presentation.jsx';
+import Admin from './Admin.jsx'
 
 export default class App extends Component {
   render() {
     return (
       <Router history={browserHistory}>
         <Route path="/:id" component={Presentation}/>
+        <Route path="/:id/admin" component={Admin}/>
       </Router>
     );
   }
