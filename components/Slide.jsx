2d1fc4c060fa59d03b665bdf61cf709ade7d0fbe
Andrew Liebchen <andrewliebchen@gmail.com>
2016-08-08 16:44:13 -0700

    Slide text/image position

--- components/Slide.jsx
+++ components/Slide.jsx
@@ -8,8 +8,12 @@ export default class Slide extends Component {
       <div className={`slide ${prefix}__slide`}>
         <div className="slide__content">
           {slide.type === 'image' ?
-            <img src={slide.source}/>
-          : <ReactMarkdown source={slide.source}/>}
+            <img
+              className="slide__content__image"
+              src={slide.source}/>
+          : <ReactMarkdown
+              className="slide__content__text"
+              source={slide.source}/>}
         </div>
         {children}
       </div>
